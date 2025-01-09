5a1987f37b6c2a702ea98e1161244dd8e1df7d09
Gasper Novak <gaspernovak22@gmail.com>
2025-01-09 11:25:44 +0100

    sln add web

--- web/obj/Debug/net8.0/web.AssemblyInfo.cs
+++ web/obj/Debug/net8.0/web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fc065d55371b8460da2e74bf7752fb4e02798f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c64d5c196784b09504171fefe5cb6c274747f717")]
 [assembly: System.Reflection.AssemblyProductAttribute("web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
