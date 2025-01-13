892a3f8c81ada99607a66ea6df38ad9fe85b4442
Gasper Novak <gaspernovak22@gmail.com>
2025-01-13 10:33:47 +0100

    Api Authorization

--- web/obj/Debug/net8.0/web.AssemblyInfo.cs
+++ web/obj/Debug/net8.0/web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6efae811aedf4836b76e9a454d4b84025c1b500")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3629e4eff0c3f46275aeb584ee7c840e1737af35")]
 [assembly: System.Reflection.AssemblyProductAttribute("web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
