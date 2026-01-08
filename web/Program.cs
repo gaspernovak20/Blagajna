65783db810d52c595074fbe7323f2b3497066e44
Gasper <gaspernovak20@gmail.com>
2026-01-08 23:52:35 +0100

    in

--- web/Program.cs
+++ web/Program.cs
@@ -31,12 +31,11 @@ var app = builder.Build();
 
 
 // Seed database using DbInitializer 
-if (app.Environment.IsDevelopment())
-{
-    using var scope = app.Services.CreateScope();
-    var context = scope.ServiceProvider.GetRequiredService<BlagajnaContext>();
-    DbInitializer.Initialize(context);
-}
+// using (var scope = app.Services.CreateScope())
+// {
+//     var context = scope.ServiceProvider.GetRequiredService<BlagajnaContext>();
+//     DbInitializer.Initialize(context);
+// }
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
